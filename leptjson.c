04b9136d09a62f0bd9da65980e7df21b09f02eaa
gatsby <gatsby.junce@gmail.com>
2020-12-21 21:15:01 +0800

    加入解析数组元素的的 API

--- leptjson.c
+++ leptjson.c
@@ -6,7 +6,7 @@
 #include <assert.h>     // assert()
 #include <errno.h>      // errno, ERANGE
 #include <math.h>       // HUGE_VAL
-#include <stdlib.h>     // NULL, strtod(), malloc(), realloc(), free()
+#include <stdlib.h>     // NULL, strtod(), malloc(), realloc(), free(), strtod()
 #include <string.h>     // memcpy()
 
 #ifndef LEPT_PARSE_STACK_INIT_SIZE
@@ -181,6 +181,41 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
     }
 }
 
+static int lept_parse_value( lept_context *c, lept_value *v );
+
+static int lept_parse_array( lept_context *c, lept_value *v ) {
+	size_t size = 0;
+	int ret;
+	EXPECT( c, '[' );
+	if( *c->json == ']' ) {
+		c->json++;
+		v->type = LEPT_ARRAY;
+		v->u.a.size = 0;
+		v->u.a.e = NULL;
+		return LEPT_PARSE_OK;
+	}
+	for( ; ; ) {
+		lept_value e;
+		lept_init( &e );
+		if( (ret = lept_parse_value( c, &e )) != LEPT_PARSE_OK )
+			return ret;
+		memcpy( lept_context_push( c, sizeof(lept_value) ), &e, sizeof(lept_value) );
+		size++;
+		if( *c->json == ',' )
+			c->json++;
+		else if( *c->json == ']' ) {
+			c->json++;
+			v->type = LEPT_ARRAY;
+			v->u.a.size = size;
+			size *= sizeof(lept_value);
+			memcpy( v->u.a.e = (lept_value*)malloc( size ), lept_context_pop( c, size ), size );
+			return LEPT_PARSE_OK;
+		}
+		else
+			return LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+	}
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':   return lept_parse_literal(c, v, "null", LEPT_NULL);
@@ -188,6 +223,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 't':   return lept_parse_literal(c, v, "true", LEPT_TRUE);
         default:    return lept_parse_number(c, v);
         case '"':   return lept_parse_string(c, v);
+        case '[':   return lept_parse_array( c, v );
         case '\0':  return LEPT_PARSE_EXPECT_VALUE;
     }
 }
@@ -264,4 +300,15 @@ void lept_set_string(lept_value* v, const char* s, size_t len) {
     v->u.s.s[len] = '\0';
     v->u.s.len = len;
     v->type = LEPT_STRING;
+}
+
+size_t lept_get_array_size( const lept_value *v ) {
+	assert( v != NULL && v->type == LEPT_ARRAY );
+	return v->u.a.size;
+}
+
+lept_value *lept_get_array_element( const lept_value *v, size_t index ) {
+	assert( v != NULL && v->type == LEPT_ARRAY );
+	assert( index < v->u.a.size );
+	return &v->u.a.e[index];
 }
