79b77f6b676013b4fdc6b09aff39b3a00b82ba2c
gatsby <gatsby.junce@gmail.com>
2020-12-24 17:36:02 +0800

    解析对象实现

--- test.c
+++ test.c
@@ -342,7 +342,7 @@ static void test_parse_miss_comma_or_curly_bracket() {
 	TEST_ERROR( LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1" );
 	TEST_ERROR( LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1]" );
 	TEST_ERROR( LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1 \"b\"" );
-	TEST_ERROR( LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\"L{}" );
+	TEST_ERROR( LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":{}" );
 }
 
 static void test_parse() {
@@ -352,9 +352,7 @@ static void test_parse() {
 	test_parse_number();
 	test_parse_string();
 	test_parse_array();
-#if 0
 	test_parse_object();
-#endif
 
 	test_parse_expect_value();
 	test_parse_invalid_value();
@@ -366,11 +364,9 @@ static void test_parse() {
 	test_parse_invalid_unicode_hex();
 	test_parse_invalid_unicode_surrogate();
 	test_parse_miss_comma_square_bracket();
-#if 1
 	test_parse_miss_key();
 	test_parse_miss_colon();
 	test_parse_miss_comma_or_curly_bracket();
-#endif
 }
 
 static void test_access_null() {
